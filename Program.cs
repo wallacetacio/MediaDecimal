2e36534cf9ed55b9f8f85235dc92f19eb4a71324
Wallace Tácio <wallacett.dev@gmail.com>
2021-11-19 21:32:41 -0300

    Adiciona resolução ao exercício Média Decimal ETEC 2021

---
+++ Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace MediaDecimal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int qnt;
+            int exb = 0;
+            decimal valor;
+            decimal soma = 0;
+            decimal media = 0;
+            decimal nMaior = 0;
+            decimal nMenor = 0;
+
+
+            Console.Write("Digite a quantidade de números: ");
+            qnt = Convert.ToInt32(Console.ReadLine());
+
+            if (qnt <= 0)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.Write("ERRO! ");
+                Console.ResetColor();
+                Console.WriteLine("Quantidade digitada deve ser maior que zero.");
+            }
+            else
+            {
+
+                while (exb < qnt)
+                {
+                    exb = exb + 1;
+
+                    Console.Write($"Número #0{exb}: ");
+
+                    valor = Convert.ToDecimal(Console.ReadLine());
+
+                    soma = soma + valor;
+
+                    media = soma / qnt;
+
+                    if (exb == 1)
+                    {
+                        nMaior = valor;
+                        nMenor = valor;
+                    }
+                    else
+                    {
+
+                        if (nMaior < valor)
+                        {
+                            nMaior = valor;
+                        }
+
+                        if (nMenor > valor)
+                        {
+                            nMenor = valor;
+                        }
+                    }
+
+                }
+                Console.WriteLine($"Soma.: {soma:N2}");
+
+                Console.WriteLine($"Média: {media:N2}");
+
+                Console.WriteLine($"Maior: {nMaior:N2}");
+
+                Console.WriteLine($"Menor: {nMenor:N2}");
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# MediaDecimal
-Exercício Média Decimal ETEC 2021
+## Exercício `MediaDecimal` ETEC 2021
+
+Receba a quantidade de números a serem digitados. Receba a quantidade indicada de números decimais. Exiba a soma e a média entre os valores digitados. Exiba também o maior e o menor número digitado.
+
+Ex.:
+```
+Quantos números: 4
+Número #01: 2,6
+Número #02: 8,5
+Número #03: 9,47
+Número #04: 0,37
+
+Soma...: 20,94
+Média..: 5,24
+Maior..: 9,47
+Menor..: 0,37
+```
